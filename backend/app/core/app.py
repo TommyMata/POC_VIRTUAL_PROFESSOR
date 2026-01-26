401ee172da490e9838f28acfdcca22dfbaec3158
RandallUCR <randallgears1998@gmail.com>
2026-01-26 11:58:53 -0600

    Expose uploads folder

--- backend/app/core/app.py
+++ backend/app/core/app.py
@@ -1,5 +1,16 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+import os
 
 def create_app() -> FastAPI:
     app = FastAPI(title="Mi API")
+
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+    UPLOADS_PATH = os.path.abspath(
+        os.path.join(BASE_DIR, "..", "..", "..", "uploads")
+    )
+
+    app.mount("/uploads", StaticFiles(directory=UPLOADS_PATH), name="uploads")
+
     return app
